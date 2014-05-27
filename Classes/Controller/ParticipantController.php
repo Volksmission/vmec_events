02b2afa6f6da2b39ba14919b0cabb417b3abe49f
Christoph Fischer <chris@toph.de>
2014-05-27 13:50:31 +0200

    Send mails when registering a participant

--- Classes/Controller/ParticipantController.php
+++ Classes/Controller/ParticipantController.php
@@ -38,7 +38,28 @@ class ParticipantController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionCont
 	 * @inject
 	 */
 	protected $participantRepository = NULL;
+	
 
+	/**
+	 * eventRepository
+	 *
+	 * @var \VMeC\VmecEvents\Domain\Repository\EventRepository
+	 * @inject
+	 */
+	protected $eventRepository = NULL;
+
+	
+	/**
+	 * inject the EventRepository object
+	 *
+	 * @param \VMeC\VmecEvents\Domain\Repository\EventRepository $eventRepository
+	 * @return void
+	 */
+	public function injectEventRepository(\VMeC\VmecEvents\Domain\Repository\EventRepository $eventRepository) {
+		$this->eventRepository = $eventRepository;
+	}
+	
+	
 	/**
 	 * action list
 	 *
@@ -77,9 +98,42 @@ class ParticipantController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionCont
 	 * @return void
 	 */
 	public function createAction(\VMeC\VmecEvents\Domain\Model\Participant $newParticipant) {
-		$this->addFlashMessage('The object was created. Please be aware that this action is publicly accessible unless you implement an access check. See <a href="http://wiki.typo3.org/T3Doc/Extension_Builder/Using_the_Extension_Builder#1._Model_the_domain" target="_blank">Wiki</a>', '', \TYPO3\CMS\Core\Messaging\AbstractMessage::ERROR);
+
+		
 		$this->participantRepository->add($newParticipant);
-		$this->redirect('list');
+
+		$eventId = $this->request->getArgument('event');
+		$event = $this->eventRepository->findByUid($eventId);
+		$event->addParticipant($newParticipant);
+		$this->eventRepository->update($event);
+		
+		$this->addFlashMessage('Deine Anmeldung wurde gespeichert. Vielen Dank.');
+		//$this->redirect('show', 'Event', null, array('event' => $event));
+		
+		// send event registration to admin
+		$email = $event->getRegistrationEmail();
+		if ($email) {
+			$this->sendTemplateEmail(array($email => 'Anmeldungen'),
+					array('noreply@volksmission.de' => 'Volksmission'),
+					'Neue Anmeldung für: '.$event->getTitle(),
+					'AnnounceRegistration',
+					array('event' => $event, 'participant' => $newParticipant)
+			);
+		}
+		
+		// send confirmation email
+		$email = $newParticipant->getEmail();
+		if ($email) {
+			$this->sendTemplateEmail(array($email => $newParticipant->getName()),
+					array('noreply@volksmission.de' => 'Volksmission'),
+					'Deine Anmeldung für: '.$event->getTitle(),
+					'ConfirmRegistration',
+					array('event' => $event, 'participant' => $newParticipant)
+			);
+		}
+		
+		
+		$this->view->assign('event', $event);
 	}
 
 	/**
@@ -126,4 +180,52 @@ class ParticipantController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionCont
 		
 	}
 
+	
+	
+	/**
+	 * Send an email from a fluid template
+	 *
+	 * @param array $recipient recipient of the email in the format array('recipient@domain.tld' => 'Recipient Name')
+	 * @param array $sender sender of the email in the format array('sender@domain.tld' => 'Sender Name')
+	 * @param string $subject subject of the email
+	 * @param string $templateName template name (UpperCamelCase)
+	 * @param array $variables variables to be passed to the Fluid view
+	 * @return boolean TRUE on success, otherwise false
+	 */
+	protected function sendTemplateEmail(array $recipient, array $sender, $subject, $templateName, array $variables = array()) {
+		$emailView = $this->objectManager->get('TYPO3\\CMS\\Fluid\\View\\StandaloneView');
+	
+		// set correct extension name
+		$emailView->getRequest()->setControllerExtensionName($this->request->getControllerExtensionName());
+	
+		$extbaseFrameworkConfiguration = $this->configurationManager->getConfiguration(\TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK);
+		$templateRootPath = \TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName($extbaseFrameworkConfiguration['view']['templateRootPath']);
+		//die ($templateRootPath);
+		$templatePathAndFilename = $templateRootPath . '/Email/' . $templateName . '.html';
+		$emailView->setTemplatePathAndFilename($templatePathAndFilename);
+		$emailView->assignMultiple($variables);
+		$emailBody = $emailView->render();
+	
+		$message = $this->objectManager->get('TYPO3\\CMS\\Core\\Mail\\MailMessage');
+		$message->setTo($recipient)
+		->setFrom($sender)
+		->setSubject($subject);
+	
+		// Possible attachments here
+		//foreach ($attachments as $attachment) {
+		//	$message->attach($attachment);
+			//}
+	
+			// Plain text example
+			//$message->setBody($emailBody, 'text/plain');
+	
+			// HTML Email
+			$message->setBody($emailBody, 'text/html');
+	
+			$message->send();
+			return $message->isSent();
+		}
+	
+	
+	
 }
